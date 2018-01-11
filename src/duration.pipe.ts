db37d4e64b0a86b49edb4ef1f6bd6c9feb974cc9
Mike Fair <mike@biblesforisrael.com>
2018-01-11 13:10:23 -0500

    add duration format pipe

--- src/duration.pipe.ts
+++ src/duration.pipe.ts
@@ -10,3 +10,13 @@ export class DurationPipe implements PipeTransform {
     return moment.duration(value, args[0] as moment.unitOfTime.DurationConstructor).humanize();
   }
 }
+
+@Pipe({ name: 'amDurationFormat' })
+export class DurationPipeFormat implements PipeTransform {
+  transform(value: any, ...args: string[]): string {
+    if (typeof args === 'undefined' || args.length !== 2) {
+      throw new Error('DurationPipe: missing required time unit argument and format string');
+    }
+    return moment.duration(value, args[0] as moment.unitOfTime.DurationConstructor).format(args[1]);
+  }
+}
